039b51ce585feaecf3c6634e89f1eb9c3dd1612e
Kosi <iyiegbuk@gmail.com>
2022-07-12 17:02:16 +0100

    setup the header/navbar component

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as PostCard } from "./postCard";
 export { default as PostWidget } from "./postWidget";
 export { default as Categories } from "./categories";
 export { default as Header } from "./header";
+export { default as Layout } from "./layout";
