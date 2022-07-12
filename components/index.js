7e0f9a3c268ce1fbeebcd54a7572eb54c38ed59e
Kosi <iyiegbuk@gmail.com>
2022-07-12 16:41:50 +0100

    setup global styles and home component

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as PostCard } from "./postCard";
+export { default as PostWidget } from "./postWidget";
+export { default as Categories } from "./categories";
+export { default as Header } from "./header";
