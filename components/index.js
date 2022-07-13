24ed0a93990a547f5818d961f76c2199e279fedd
Kosi <iyiegbuk@gmail.com>
2022-07-13 14:02:36 +0100

    setup the request to get post details in a page

--- components/index.js
+++ components/index.js
@@ -3,3 +3,7 @@ export { default as PostWidget } from "./postWidget";
 export { default as Categories } from "./categories";
 export { default as Header } from "./header";
 export { default as Layout } from "./layout";
+export { default as Author } from "./author";
+export { default as Comments } from "./comments";
+export { default as CommentsForm } from "./commentsForm";
+export { default as PostDetail } from "./postDetail";
