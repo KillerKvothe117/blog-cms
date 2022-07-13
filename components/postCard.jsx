2c858b7271eb5ff186c020506ea9a12d43abc7a7
Kosi <iyiegbuk@gmail.com>
2022-07-13 11:45:58 +0100

    completed the post widget

--- components/postCard.jsx
+++ components/postCard.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import moment from "moment";
 import Link from "next/link";
@@ -9,24 +8,28 @@ const PostCard = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-        <img
-          src={post.featuredImage.url}
-          alt={post.title}
-          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
-        />
+        <picture>
+          <img
+            src={post.featuredImage.url}
+            alt={post.title}
+            className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+          />
+        </picture>
       </div>
       <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-purple-600 text-3xl font-semibold">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
         <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-          <img
-            alt={post.author.name}
-            height="50px"
-            width="50px"
-            className="align-middle rounded-full"
-            src={post.author.photo.url}
-          />
+          <picture>
+            <img
+              alt={post.author.name}
+              height="50px"
+              width="50px"
+              className="align-middle rounded-full"
+              src={post.author.photo.url}
+            />
+          </picture>
           <p className="inline align-middle text-gray-700 ml-2 text-lg">
             {post.author.name}
           </p>
