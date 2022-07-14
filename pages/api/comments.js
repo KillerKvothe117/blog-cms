66d224c69a1e91506dba5fcab5ef819b84fbefaf
Kosi <iyiegbuk@gmail.com>
2022-07-14 10:59:36 +0100

    done with UI for posting comments

---
+++ pages/api/comments.js
@@ -0,0 +1,15 @@
+/** *************************************************************
+ * Any file inside the folder pages/api is mapped to /api/* and  *
+ * will be treated as an API endpoint instead of a page.         *
+ *************************************************************** */
+import { gql, GraphQLClient } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+
+export default function comments(req, res) {
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      // authorization: `Bearer ${process.env.}`
+    }
+  })
+}
