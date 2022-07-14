691ef844eb2c6eedea891acfdededbf678115b4b
Kosi <iyiegbuk@gmail.com>
2022-07-14 11:08:57 +0100

    setup endpoint for submitting comments

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -9,7 +9,17 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 export default function comments(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      // authorization: `Bearer ${process.env.}`
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
     }
   })
+
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) {id}
+    }
+  `
+
+  const result = await graphQLClient.request(query, req.body)
+
+  return res.status(200).send(result)
 }
